7909af5001997b40052c4d5ace658341336d20bb
BlackBoxJU50 <ahmedhasib2224@gmail.com>
2026-06-28 00:40:08 +0600

    Completed CRUD operations for HRM Module

--- apps/hrm/urls.py
+++ apps/hrm/urls.py
@@ -6,5 +6,7 @@ app_name= 'hrm'
 urlpatterns = [
     path('employees/', views.employee_list_view, name='employee_list'),
     path('employees/add/', views.employee_create_view, name='employee_add'),
-      
+    path('employees/<int:pk>/edit/', views.employee_edit_view, name='employee_edit'),
+    path('employees/<int:pk>/delete/', views.employee_delete_view, name='employee_delete'),
+    
 ]   
--- apps/hrm/views.py
+++ apps/hrm/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
 from .models import Employee
@@ -11,7 +12,7 @@ def employee_list_view(request):
     }
     return render(request, 'hrm/employee_list.html', context)
 
-    
+
 @login_required(login_url='login')
 def employee_create_view(request): 
 
@@ -23,3 +24,30 @@ def employee_create_view(request):
     else: 
         form = EmployeeForm() 
     return render(request, 'hrm/employee_form.html', {'form': form}) 
+
+@login_required(login_url='login')
+def employee_edit_view(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('hrm:employee_list')
+    else:
+        form = EmployeeForm(instance=employee)
+    return render(request, 'hrm/employee_form.html', {'form': form})
+
+@login_required(login_url='login')
+def employee_delete_view(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('hrm:employee_list')
+    return render(request, 'hrm/employee_confirm_delete.html', {'employee': employee})
+
+
+
+
+    
+        
+    
